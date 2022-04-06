c28b88f13b5f75bb1c956110689075f362df48d4
Sarah Indano <sarah.indano@gmail.com>
2022-04-06 16:50:12 -0400

    commit tree e12823826394d854e5a6520bd31a6efd1d850eec

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require 'pry'
+class CashRegister
+ attr_accessor :total, :discount, :items, :last_transaction
 
+ def initialize(total = 0, discount=20)
+    @total = total
+    @discount = discount
+    @items = []
+ end
+
+ def add_item(title, cost, quantity=1)
+    self.last_transaction = cost * quantity
+    self.total += self.last_transaction
+    self.items << total
+end
+
+end
+
+order1 = CashRegister.new(0.00)
+order2 = CashRegister.new(1.00)
